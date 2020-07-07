9d73eac4f08fca5f2a262c8c3461ba9c795285b1
AshaR-246 <62698273+AshaR-246@users.noreply.github.com>
2020-07-07 14:03:54 +0530

    Add files via upload

---
+++ WhiteHat Jr. Dj/sound-mixer-stage-_---1/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { View, Button, Alert } from 'react-native';
+
+
+export default class App extends Component { 
+  render(){
+    return(
+      <View>
+        <View 
+          style={{ width:200 , height:100, marginTop:80}}>
+          <Button 
+            title="sound1"  
+            color = "red"
+            onPress={() => Alert.alert('Play Sound 1.m4as')} />
+        </View>
+        <View
+          style={{ width:200 , height:100, marginTop:20, marginLeft:150}}>
+          <Button 
+            title="sound2"  
+            color = "blue"
+            onPress={() => Alert.alert('Play Sound 2')} />
+        </View>
+        <View
+          style={{ width:180 , height:100, marginTop:20}}>
+          <Button 
+            title="sound3"  
+            color = "purple"
+            onPress={() => Alert.alert('Play Sound 3')} />
+        </View>
+        <View 
+          style={{ width:180 , height:100, marginTop:20, marginLeft:80, marginRight:80}}>
+          <Button 
+            title="sound4"  
+            color = "black"
+            onPress={() => Alert.alert('Play Sound 4')} />
+        </View>
+      <View
+     
+       style={{ width:100 , height:100, marginTop:280, marginLeft:50, marginRight:50}}>
+          <Button 
+            title="Stop Sound"  
+            color = "pink"
+           onPress={() => Alert.alert('Stop Sound')} />
+        </View>
+      </View>
+
+    )
+  }
+}
